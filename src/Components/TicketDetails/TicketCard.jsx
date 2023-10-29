5d6452e90c7c3efbfda0741dc8cdac52fdebed9b
MohammedMaher177 <anahammo777@gmail.com>
2023-10-29 21:58:20 +0200

    some fixes

---
+++ src/Components/TicketDetails/TicketCard.jsx
@@ -0,0 +1,90 @@
+import {
+  Badge,
+  Box,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from "@mui/material";
+import LensIcon from "@mui/icons-material/Lens";
+
+import styles from "./ticketDetails.module.css";
+
+export default function TicketCard() {
+  const shapeStyles = { bgcolor: "#e6ee9c", width: 80, height: 40 };
+  const shapeCircleStyles = { borderRadius: "8px" };
+
+  const circle = (
+    <Box component="span" sx={{ ...shapeStyles, ...shapeCircleStyles }} />
+  );
+  return (
+    <>
+      <ListItemButton
+        sx={{
+          pl: 4,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 2 }}>
+          <ListItemText
+            primary="User Name"
+            secondary={
+              <Badge
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                color="default"
+                overlap="circular"
+                badgeContent="Pending"
+                anchorOrigin={{
+                  vertical: "center",
+                  horizontal: "center",
+                }}
+              >
+                {circle}
+              </Badge>
+            }
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          />
+          <ListSubheader
+            component="div"
+            id="nested-list-subheader"
+            className={styles.sub_header}
+          >
+            27 Jon 2023
+            <LensIcon
+              sx={{
+                color: "#D9D9D9",
+                width: "5px",
+                height: "5px",
+                marginInline: 1,
+              }}
+            />
+            <Typography sx={{ whiteSpace: "nowrap" }}>
+              From: Department
+            </Typography>
+            <Typography sx={{ whiteSpace: "nowrap" }}>
+              To: Department
+            </Typography>
+            <Typography sx={{ whiteSpace: "nowrap" }}>Branch</Typography>
+          </ListSubheader>
+        </Box>
+        <ListItemIcon>
+          <LensIcon
+            sx={{
+              color: "#D9D9D9",
+              width: "50px",
+              height: "50px",
+              marginRight: 1,
+            }}
+          />
+        </ListItemIcon>
+      </ListItemButton>
+    </>
+  );
+}
