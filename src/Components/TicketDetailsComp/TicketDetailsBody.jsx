d9a6af576bbff8d31ec3f0a6721058a195f16682
MohammedMaher177 <anahammo777@gmail.com>
2023-10-31 17:50:45 +0200

    pdf'

--- src/Components/TicketDetailsComp/TicketDetailsBody.jsx
+++ src/Components/TicketDetailsComp/TicketDetailsBody.jsx
@@ -18,28 +18,38 @@ export default function TicketDetailsBody() {
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry.
         <br />
-        <br /> Lorem Ipsum has been the industry standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book not only fiv...
+        <br /> Lorem Ipsum has been the industry standard dummy text ever since
+        the 1500s, when an unknown printer took a galley of type and scrambled
+        it to make a type specimen book not only fiv...
         <br />
         <br /> It has survived not only five centuries, but also the leap into
         electronic typesetting, remaining essentially unchanged.
       </Typography>
-      
-        <ListItemText
-          primary="Attachments"
-          secondary={`(2)`}
-          sx={{
-            fontSize: "20px",
-            lineHeight: "32px",
-            letterSpacing: "0.15px",
-            fontWeight: "600",
-            fontFamily: "Roboto",
-            color: "#001011",
-            display: "flex",
-            gap: 1,
-          }}
-        />
+
+      <ListItemText
+        primary="Attachments"
+        secondary={`(2)`}
+        sx={{
+          fontSize: "20px",
+          lineHeight: "32px",
+          letterSpacing: "0.15px",
+          fontWeight: "600",
+          fontFamily: "Roboto",
+          color: "#001011",
+          display: "flex",
+          gap: 1,
+        }}
+      />
+
+      <div className="col-md-4 col-sm-12 col-xs-12 text-right wow fadeInRight animated animated">
+        <a
+          target="_blank"
+          href="/src/assets/Technical_Task.pdf"
+          className="download-btn theme-btn"
+        >
+          DOWNLOAD .PDF programme
+        </a>
+      </div>
     </>
   );
 }
--- src/Pages/TicketDetails/TicketDetails.jsx
+++ src/Pages/TicketDetails/TicketDetails.jsx
@@ -33,6 +33,7 @@ export default function TicketDetails() {
     getTickets();
     toggleDrawer();
     return openS;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
